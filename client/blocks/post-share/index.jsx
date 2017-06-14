a14bdb6c1cc413f870d246e63c3420f126a03d81
retrofox <rdsuarez@gmail.com>
2017-06-14 09:08:21 -0300

    post-share: add `disabled` property

--- client/blocks/post-share/index.jsx
+++ client/blocks/post-share/index.jsx
@@ -71,6 +71,7 @@ class PostShare extends Component {
 		// parent prps
 		post: PropTypes.object,
 		siteId: PropTypes.number,
+		disabled: PropTypes.bool,
 
 		// connect prps
 		businessDiscountedRawPrice: PropTypes.number,
@@ -92,6 +93,7 @@ class PostShare extends Component {
 
 	static defaultProps = {
 		connections: [],
+		disabled: false,
 	};
 
 	state = {
@@ -196,6 +198,7 @@ class PostShare extends Component {
 
 	renderSharingButtons() {
 		const {
+			disabled,
 			hasRepublicizeSchedulingFeature,
 			siteId,
 			translate,
@@ -205,7 +208,7 @@ class PostShare extends Component {
 			className="post-share__button"
 			primary
 			onClick={ this.sharePost }
-			disabled={ this.isSharingPost() }
+			disabled={ this.isSharingPost() || disabled }
 		>
 			{ this.state.scheduledDate ? translate( 'Schedule post' ) : translate( 'Share post' ) }
 		</Button>;
@@ -222,6 +225,7 @@ class PostShare extends Component {
 			<div className="post-share__button-actions">
 				{ ( isEnabled( 'publicize-preview' ) ) &&
 					<Button
+						disabled={ disabled }
 						className="post-share__preview-button"
 						onClick={ this.toggleSharingPreview }
 					>
@@ -233,13 +237,14 @@ class PostShare extends Component {
 					className="post-share__share-combo"
 					primary
 					busy={ this.isSharingPost() }
+					disabled={ disabled }
 				>
 					{ shareButton }
 
 					<CalendarButton
 						primary
 						className="post-share__schedule-button"
-						disabled={ this.isSharingPost() }
+						disabled={ this.isSharingPost() || disabled }
 						title={ translate( 'Set date and time' ) }
 						selectedDay={ this.state.scheduledDate }
 						tabIndex={ 3 }
